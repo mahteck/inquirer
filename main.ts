314c47deeaceb5471c7294f67186037705079096
mshoaib1244 <shoaib1244@gmail.com>
2024-04-20 16:47:23 +0500

    second commit multiple questions

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
 import inquirer from "inquirer";
 
-let answers = await inquirer.prompt({
-    name: "age",
-    type: "number",
-    message: "Enter your age",
-});
-console.log("insha Allah in " + (60 - answers.age) + "years you will be 60 years old.");
+let answers = await inquirer.prompt(
+    [
+        {
+            name: "Q1",
+            type: "input",
+            message: "Enter your Name",
+        },
+        {
+            name: "Q2",
+            type: "list",
+            message: "Enter your Gender",
+            choices: ["Male", "Female"]
+        }
+    ]
+);
+console.log(`hello ${answers.Q1} and your gender is ${answers.Q2}`);
+
+//console.log("insha Allah in " + (60 - answers.age) + "years you will be 60 years old.");
